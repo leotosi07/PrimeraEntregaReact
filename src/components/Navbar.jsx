32007a8b148d473a4d6fd1c9967407a6db2ef35f
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2024-09-23 20:16:19 -0300

    inclusion de incono

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
+            <img src="/logo.png" alt="Logo" width=" " height="30" className="d-inline-block align-top" />
                 <a className="navbar-brand" href="#">Pizzería de Andrea</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
